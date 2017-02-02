ab993328421fb45e344a137ccb45f1083425485a
HeasHeartfire <gagi1995@gmail.com>
2017-02-02 18:55:10 +0100

    Subir archivos

--- resources/views/catalog/show.blade.php
+++ resources/views/catalog/show.blade.php
@@ -3,16 +3,19 @@
 @section('content')
 
     <div class="row">
-	    <div class="col-sm-4">
-			
-			<h2>{{$pelicula['title']}}</h2>
+	    <div class="col-sm-4">	
 	        <img src="{{$pelicula['poster']}}"/>
-
 	    </div>
 	    <div class="col-sm-8">
-
-	        {{-- TODO: Datos de la película --}}
-
+			
+	        	<h2>{{$pelicula['title']}}</h2>
+	        	<h4>Año: {{$pelicula['year']}}</h4>
+	        	<h4>Director: {{$pelicula['director']}}</h4>
+	        	<p><strong>Resumen:</strong> {{$pelicula['synopsis']}}</p>
+	        	<p><strong>Estado:</strong> 
+	        				@if ($pelicula['rented']) Película ya alquilada
+	        				@else Película disponible
+	        				@endif</p>
 	    </div>
 	</div>
 
